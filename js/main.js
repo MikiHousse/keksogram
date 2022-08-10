d61ff1e83b0b62c794304f256713dfd723995a2c
MikiHousse <109237708+MikiHousse@users.noreply.github.com>
2022-08-10 19:39:33 +0300

    Загрузили проект

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+const isMin = 5;
+// eslint-disable-next-line no-unused-vars
+const isMax = 10;
+
+const Number = function (min, max) {
+  if (min > 10 || max < 10) {
+    return true;
+  }
+  return false;
+}
+
+// eslint-disable-next-line no-console
+console.log (Number);
