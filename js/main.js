a32770e78cd33cf260adf0aa71905eafd8023ac8
MikiHousse <mikichaoss@gmail.com>
2022-08-11 20:20:01 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-function  getRundom(min, max) {
+const getRandom = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
   return Math.floor(Math.random()*(max - min + 1)) + min;
-}
+};
 
-getRundom(1,10);
+(getRandom(1, 10));
+
+const stringCount = (text, count) => {
+  return text.lenght <= count;
+};
+
+// eslint-disable-next-line no-console
+console.log(stringCount('Это новый текст', 100));
 
 let socialFooterText = document.querySelector('.social__footer-text');
 let socialFooter = document.querySelector('.social__footer');
