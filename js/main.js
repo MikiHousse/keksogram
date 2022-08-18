403c014e43dfdecbd54ad50a70a07cd753912daa
MikiHousse <mikichaoss@gmail.com>
2022-08-18 18:59:45 +0300

    Сделал третью часть практики

--- js/main.js
+++ js/main.js
@@ -1,35 +1,15 @@
-const getRandom = (min, max) => {
-  if (min < 0 || max < 0) {
-    return -1;
-  }
+import {photos} from './data.js';
 
-  if (max < min) {
-    [min, max] = [max, min];
-  }
 
-  return Math.floor(Math.random()*(max - min + 1)) + min;
-};
+console.log(photos);
 
-(getRandom(1, 10));
 
-const stringCount = (text, count) => {
-  return text.lenght <= count;
-};
 
-// eslint-disable-next-line no-console
-console.log(stringCount('Это новый текст', 100));
 
-let socialFooterText = document.querySelector('.social__footer-text');
-let socialFooter = document.querySelector('.social__footer');
-let socialFooterBtn = document.querySelector('.social__footer-btn');
 
-socialFooterText.oninput = function () {
-  // console.log(socialFooter.value);
-  if (socialFooterText.value.lenght > 100) {
-    socialFooter.classList.add('warning');
-    socialFooterBtn.disabled = true;
-  } else {
-    socialFooter.classList.remove('warning');
-    socialFooterBtn.disabled = false;
-  }
-};
+
+
+
+
+
+
---
+++ js/util.js
@@ -0,0 +1,28 @@
+'use stric'
+// Случайное число
+const getRandomInt = function (min, max) {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+// Проверка строки
+const stringCount = (text, sing) => {
+  return text.lenght <= sing ? true : false;
+};
+
+console.log(stringCount('Это новый текст', 100));
+
+// Случайный элемент масива
+const getRandomElementArr = (array) => {
+  return array[getRandomInt(0, array.length - 1)];
+}
+
+export { getRandomInt, stringCount, getRandomElementArr };
